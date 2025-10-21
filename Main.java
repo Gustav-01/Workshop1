20a42c4353e609797f65fdede1184244b4d38981
MegamackDaCodemaster <ac75115@skola.goteborg.se>
2025-10-21 13:47:27 +0200

    Changed the first line 'Mackan'

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from Person B!"); //Ändring av PersonB
+        System.out.println("Hello from Mackan!"); //Ändring av PersonB
     }
 }
